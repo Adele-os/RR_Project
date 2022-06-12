e90e5f45de48a75c19450242e925f7212affbd54
Saeed Pourjafar <41834810+SaeedPourjafar@users.noreply.github.com>
2022-06-13 00:43:38 +0200

    Update comparitive_analysis.R
    func.R now can be used for all sex, age and experience

--- src/comparitive_analysis.R
+++ src/comparitive_analysis.R
@@ -1,6 +1,9 @@
 library(dplyr)
 library(RVAideMemoire) # library for Mood's median test
 library(rcompanion)    # library for pairwise Mood's median tests
+library(types)         # library for type annotation in functions
+
+source("src/func.R")   # read our function from file func.R
 
 # Import data 
 data1 <-  read.csv("in/survey_data.csv", header = T)
@@ -28,35 +31,7 @@ for(i in 1:(n-1)){
 data.boot2$age[data.boot2$age == 4] <- 3
 
 
-# sex comparison (2 samples test) -----------------------------------------
-
-# a function for running pairwise Mood's test for all n Bootstrap samples 
-# It takes the dataframe, name of variable (column) to compare
-# and name of column which includes the group each case assigned to. 
-
-bootstrap.p <- function(df, col.name, col.group, n=50){
-  
-  # create an empty dataframe for saving the results of each loop in one row
-  result  <- data.frame(0, 0, 'Z statistics'= 0, 'p' = 0)
-  
-  # a for-loop for each sample
-  for (i in 1:n) {
-    
-    # extract ith sample from whole dataset
-    y <- df[((i-1)*N+1):(N*i), col.name] # assign our dependent variable to y
-    x <- df[((i-1)*N+1):(N*i), col.group] # assign our independent variable to y
-    
-    # assign the result of ith sample to ith row of "result"
-    result[i,] <- c(
-      (aggregate(y, list(x), median, na.rm=T))$x, #calculate median for each group
-      mood.test(y ~ x)$statistic,                 # extract test statistic of Mood's test
-      mood.test(y ~ x)$p                          # extract p-value
-      )
-  }
-  
-  # return a vector contains mean of each column of "result"
-  return(apply(result,2,mean))
-}
+# sex comparison -----------------------------------------
 
 # using our "bootstrap.p" function for camparing males and females:
 
@@ -99,10 +74,13 @@ table1 <- rbind(
     )
 
 # move the column of names from last column to first 
-table1 <- table1[,c(5,1:4)]
+table1 <- table1[,c(4,1:3)]
 
 # add the names of each column
-names(table1) <- c("Fallacy", "Female", "Male", "Test-statistics", "p-Value")
+names(table1) <- c("Fallacy", "Female", "Male", "p-Value")
 
 # print table1
 table1
+
+
+
