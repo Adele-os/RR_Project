20f645b36b0d20511bf0bd3608c1ba4818162cfa
Adele Ossareh <79600636+Adele-os@users.noreply.github.com>
2022-06-13 17:12:53 +0200

    age comparison added

--- src/comparitive_analysis.R
+++ src/comparitive_analysis.R
@@ -66,12 +66,12 @@ table1 <- rbind(
   round(p.b_hot_hand.sex, 3),
   round(p.b_bandwagon.sex, 3),
   round(p.b_dunning.sex, 3)
-  ) %>% 
+) %>% 
   as.data.frame() %>%         # convert matrix to dataframe
   mutate(                     # add a column including the name of each row
     Fallacies = c("Confirmation", "Loss aversion", "Gambler's", "Availability",
                   "Hot hand's", "Bandwagon", "Dunning-Kruger")
-    )
+  )
 
 # move the column of names from last column to first 
 table1 <- table1[,c(4,1:3)]
@@ -81,6 +81,54 @@ names(table1) <- c("Fallacy", "Female", "Male", "p-Value")
 
 # print table1
 table1
+
+
+# age comparison ----------------------------------------------------------
+
+## Confirmation Bias
+b_confirmation.age <- bootstrap.p(data.boot2, 'b_confirmation', 'age')
+
+## Loss aversion
+b_loss.age <- bootstrap.p(data.boot2, 'b_loss', 'age')
+
+## Gambler's fallacy
+b_gambler.age <- bootstrap.p(data.boot2, 'b_gambler', 'age')
+
+## Availability Cascade
+b_availability.age <- bootstrap.p(data.boot2, 'b_availability', 'age')
+
+## Hot hand's fallacy
+b_hot_hand.age <- bootstrap.p(data.boot2, 'b_hot_hand', 'age')
+
+## Bandwagon effect
+b_bandwagon.age <- bootstrap.p(data.boot2, 'b_bandwagon', 'age')
+
+## Dunning-Kruger effect
+b_dunning.age <- bootstrap.p(data.boot2, 'b_dunning', 'age')
+
+# put all the results in a table
+table2 <- rbind(
+  round(b_confirmation.age, 3),
+  round(b_loss.age, 3),
+  round(b_gambler.age, 3),
+  round(b_availability.age, 3),
+  round(b_hot_hand.age, 3),
+  round(b_bandwagon.age, 3),
+  round(b_dunning.age, 3)
+) %>% 
+  as.data.frame() %>%          # convert matrix to dataframe
+  mutate(                      # add a column including the name of each row
+    Fallacies <- c("Confirmation", "Loss aversion", "Gambler's", "Availability",
+                   "Hot hand's", "Bandwagon", "Dunning-Kruger")
+  )
+
+# move the column of names from last column to first 
+table2 <- table2[,c(5,1:4)]
+
+# add names of each column
+names(table2) <- c("Fallacy", "<35", "36-45", ">45", "p-Value")
+
+table2
 
 
 
